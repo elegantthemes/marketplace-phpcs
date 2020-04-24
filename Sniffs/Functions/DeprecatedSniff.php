b1f9a36493ddea1e7028276227215ed26fe12cb7
Miguel Sirvent <miguel.sirvent@gmail.com>
2020-04-24 14:36:39 -0500

    Updated deprecated sniff

--- Sniffs/Functions/DeprecatedSniff.php
+++ Sniffs/Functions/DeprecatedSniff.php
@@ -7,13 +7,18 @@ use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\ForbiddenFunctionsSniff;
 class DeprecatedSniff extends ForbiddenFunctionsSniff {
 
 	public $forbiddenFunctions = [
-		'et_sanitize_value_by_cap'      => 'et_core_sanitize_value_by_cap',
-		'et_intentionally_unsanitized'  => 'et_core_intentionally_unsanitized',
-		'et_intentionally_unescaped'    => 'et_core_intentionally_unescaped',
-		'et_esc_wp'                     => 'et_core_esc_wp',
-		'et_esc_previously'             => 'et_core_esc_previously',
-		'et_fb_enabled'                 => 'et_core_is_fb_enabled',
-		'et_pb_generate_responsive_css' => 'ET_Builder_Module_Helper_ResponsiveOptions::instance()->generate_responsive_css()',
+		'et_sanitize_value_by_cap'            => 'et_core_sanitize_value_by_cap',
+		'et_intentionally_unsanitized'        => 'et_core_intentionally_unsanitized',
+		'et_intentionally_unescaped'          => 'et_core_intentionally_unescaped',
+		'et_esc_wp'                           => 'et_core_esc_wp',
+		'et_esc_previously'                   => 'et_core_esc_previously',
+		'et_fb_enabled'                       => 'et_core_is_fb_enabled',
+		'et_pb_generate_responsive_css'       => 'ET_Builder_Module_Helper_ResponsiveOptions::instance()->generate_responsive_css()',
+		'et_core_api_email_providers'         => 'ET_Core_API_Email_Providers::instance()',
+		'et_pb_get_standard_post_types'       => 'ET_Builder_Library::built_for_post_types()',
+		'et_pb_get_used_built_for_post_types' => 'ET_Builder_Library::built_for_post_types(\'all\')',
+		'et_is_gutenberg_active'              => 'et_core_is_gutenberg_active()',
+		'et_is_gutenberg_enabled'             => 'et_core_is_gutenberg_enabled()',
 	];
 
 	protected function addError( $phpcsFile, $stackPtr, $function, $pattern = null ) {
