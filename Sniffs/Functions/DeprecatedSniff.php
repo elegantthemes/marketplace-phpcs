29cd364fe833765dfb21a1c662473ca2a3f1a5a9
Miguel Sirvent <miguel.sirvent@gmail.com>
2020-04-30 22:18:01 -0500

    Fixed double ()() on deprecated warning.

--- Sniffs/Functions/DeprecatedSniff.php
+++ Sniffs/Functions/DeprecatedSniff.php
@@ -7,12 +7,12 @@ use PHP_CodeSniffer\Standards\Generic\Sniffs\PHP\ForbiddenFunctionsSniff;
 class DeprecatedSniff extends ForbiddenFunctionsSniff {
 
 	public $forbiddenFunctions = [
-		'et_sanitize_value_by_cap'            => 'et_core_sanitize_value_by_cap',
-		'et_intentionally_unsanitized'        => 'et_core_intentionally_unsanitized',
-		'et_intentionally_unescaped'          => 'et_core_intentionally_unescaped',
-		'et_esc_wp'                           => 'et_core_esc_wp',
-		'et_esc_previously'                   => 'et_core_esc_previously',
-		'et_fb_enabled'                       => 'et_core_is_fb_enabled',
+		'et_sanitize_value_by_cap'            => 'et_core_sanitize_value_by_cap()',
+		'et_intentionally_unsanitized'        => 'et_core_intentionally_unsanitized()',
+		'et_intentionally_unescaped'          => 'et_core_intentionally_unescaped()',
+		'et_esc_wp'                           => 'et_core_esc_wp()',
+		'et_esc_previously'                   => 'et_core_esc_previously()',
+		'et_fb_enabled'                       => 'et_core_is_fb_enabled()',
 		'et_pb_generate_responsive_css'       => 'ET_Builder_Module_Helper_ResponsiveOptions::instance()->generate_responsive_css()',
 		'et_core_api_email_providers'         => 'ET_Core_API_Email_Providers::instance()',
 		'et_pb_get_standard_post_types'       => 'ET_Builder_Library::built_for_post_types()',
@@ -27,7 +27,7 @@ class DeprecatedSniff extends ForbiddenFunctionsSniff {
 		}
 		$data   = [ $function ];
 		$data[] = $this->forbiddenFunctions[ $pattern ];
-		$error  = 'The function %s() is deprecated; Please use %s() instead';
+		$error  = 'The function %s() is deprecated; Please use %s instead';
 		$phpcsFile->addError( $error, $stackPtr, 'FoundWithAlternative', $data );
 	}
 }
