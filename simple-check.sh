96c346f3a0f8fd8663293b81fef3fa8903b24766
Hridoy Mozumder <hrrarya6@gmail.com>
2022-02-19 11:58:16 +0600

    adding check for woo-essential/template/woocommerce-template folder

--- simple-check.sh
+++ simple-check.sh
@@ -1,10 +1,20 @@
 #!/bin/bash          
 read -p 'Enter your module folder path(avoid last Oblique): ' path
-
+woo_template="$path/templates/woocommerce"
 
 for singlepath in "$path/includes/modules/*/**.php"
 do
     ./vendor/bin/phpcs --standard=ruleset.xml ${singlepath}
 done
+
+
+
+if [[ -d ${woo_template} ]]
+then
+	for template_path in "$woo_template/*/**.php"
+	do 
+		./vendor/bin/phpcs --standard=ruleset.xml ${template_path}
+	done
+fi 
 
 echo 'all done'
