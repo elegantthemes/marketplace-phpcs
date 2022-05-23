3e9025a768885583b8655f9475285c40095dda4b
Lanre Smith <abims212@gmail.com>
2022-05-23 12:56:40 +0100

    Fix styling issues

--- Sniffs/UpdateURISniff.php
+++ Sniffs/UpdateURISniff.php
@@ -18,22 +18,28 @@ class UpdateURISniff implements Sniff {
 	/**
 	 * Processes the tokens that this sniff is interested in.
 	 *
-	 * @param File $phpcsFile                 The file where the token was found.
-	 * @param int  $stackPtr                  The position in the stack where
-	 *                                        the token was found.
+	 * @param File $phpcsFile The file where the token was found.
+	 * @param int  $stackPtr  The position in the stack where the token was found.
 	 *
 	 * @return void
 	 */
 	public function process( File $phpcsFile, $stackPtr ) {
-        $tokens = $phpcsFile->getTokens();
+		$tokens = $phpcsFile->getTokens();
 
 		if ( ! $this->isETUpdateURI( $tokens[ $stackPtr ]['content'] ) ) {
-			$phpcsFile->addError( "Invalid plugin update URI", $stackPtr, 'InvalidUpdateURI' );
+			$phpcsFile->addError( "Invalid plugin update URI. Plugins submitted to Elegant Themes Marketplace should use https://elegantthemes.com to avoid conflict with plugins sold on seller's website.", $stackPtr, 'InvalidUpdateURI' );
 		}
-    }
+	}
 
-    public function isETUpdateURI( $content ) {
-        return ( 0 === preg_match( '/Update URI\s*:/i', $content ) )
-            || ( 1 === preg_match( '/Update URI\s*:\s*(https?:\/\/)?([a-z0-9]+\.)*elegantthemes.com/i', $content ) );
-    }
+	/**
+	 * Checks if the 'Update URI' header is empty or set to ET's.
+	 *
+	 * @param string $content Comment content.
+	 *
+	 * @return bool
+	 */
+	public function isETUpdateURI( $content ) {
+		return ( 0 === preg_match( '/Update URI\s*:/i', $content ) )
+			|| ( 1 === preg_match( '/Update URI\s*:\s*(https?:\/\/)?([a-z0-9]+\.)*elegantthemes.com/i', $content ) );
+	}
 }
